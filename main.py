885d43f6ccea3dcaeb160d7d2a8b837effe43f98
Nastenka <nastenka.pazdnikova@yandex.ru>
2022-09-10 18:46:49 +0300

    сделала включение и выключение по хлопку

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from machine import Pin
 from time import sleep
 
-def wait_for_clap(mic):
+def wait_for_number_of_claps(mic, claps_count, claps_delay):
+
     while not mic.value():
-        sleep(0.01)
+        sleep(0.0001)
+
+    sleep(claps_delay)
+
+    wait_timeout = claps_count * claps_delay
+
+    while not mic.value() and wait_timeout > 0:
+        sleep(0.001)
+        wait_timeout = wait_timeout - 0.001
+
+    return wait_timeout > 0
 
 
 mic = Pin(10, Pin.IN, Pin.PULL_UP)
@@ -12,9 +23,15 @@ lampa = Pin(11, Pin.OUT)
 
 
 while True:
-    wait_for_clap(mic)
+    while not wait_for_number_of_claps(mic, 2, 0.2):
+        pass
+
     lampa.high()
+    sleep(0.5)
 
-    wait_for_clap(mic)
+    while not wait_for_number_of_claps(mic, 2, 0.2):
+        pass
+
+
     lampa.low()
-
+    sleep(0.5)
