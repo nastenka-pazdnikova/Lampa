0aab5ed6148c5031d79a60493d664f78a4d0c20c
Nastenka <nastenka.pazdnikova@yandex.ru>
2022-09-13 18:22:38 +0300

    добавила последовательности хлопкам

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from machine import Pin
 from time import sleep
 
-def wait_for_number_of_claps(mic, claps_count, claps_delay):
+from machine import Pin
 
+
+def wait_for_number_of_claps(mic, claps_count, claps_delay):
     for i in range(claps_count):
 
         wait_timeout = claps_delay
@@ -25,21 +26,65 @@ def wait_for_number_of_claps(mic, claps_count, claps_delay):
     return True
 
 
+def count_number_of_claps(mic, claps_delay):
+    while not mic.value():
+        sleep(0.001)
+
+    for delay in claps_delay:
+        sleep(delay)
+
+        wait_timeout = 0.2
+        while wait_timeout > 0:
+
+            if mic.value():
+                break
+
+            sleep(0.001)
+            wait_timeout = wait_timeout - 0.001
+
+        if not wait_timeout > 0:
+            return False
+
+        print(f"cought clap {delay}")
+
+    print(f"catch claps {claps_delay}")
+
+    return True
+
+
 mic = Pin(10, Pin.IN, Pin.PULL_UP)
 lampa = Pin(11, Pin.OUT)
 
 
+def exact_number_of_claps():
+    while True:
+        while not wait_for_number_of_claps(mic, 3, 0.2):
+            pass
 
-while True:
-    while not wait_for_number_of_claps(mic, 3, 0.2):
-        pass
+        lampa.high()
+        sleep(0.5)
 
-    lampa.high()
-    sleep(0.5)
+        while not wait_for_number_of_claps(mic, 2, 0.2):
+            pass
 
-    while not wait_for_number_of_claps(mic, 2, 0.2):
-        pass
+        lampa.low()
+        sleep(0.5)
 
 
-    lampa.low()
-    sleep(0.5)
+def dynamic_claps():
+    while True:
+
+        while not count_number_of_claps(mic, [0.2, 0.5]):
+            pass
+
+        lampa.high()
+        sleep(0.5)
+
+        while not count_number_of_claps(mic, [0.5, 0.2]):
+            pass
+
+        lampa.low()
+        sleep(0.5)
+
+
+dynamic_claps()
